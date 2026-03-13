64ff052af58c91439737b82605392d419591f6f8
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-13 19:46:12 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,16 +1,3 @@
 function login(){
-
-let email = document.getElementById("email").value;
-let senha = document.getElementById("senha").value;
-
-if(email === "teste@email.com" && senha === "123456"){
-
-document.getElementById("msg").innerText = "Login funcionando";
-
-}else{
-
-document.getElementById("msg").innerText = "Email ou senha errado";
-
-}
-
+alert("JS funcionando");
 }
