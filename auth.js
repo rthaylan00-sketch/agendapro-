58e9e990e3d57888d05bd200738af95de3ccde6d
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-13 11:28:45 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,32 @@
+import { auth } from "./firebase.js";
 
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+function login(){
+
+const email = document.getElementById("email").value;
+
+const senha = document.getElementById("senha").value;
+
+
+signInWithEmailAndPassword(auth, email, senha)
+
+.then(() => {
+
+window.location.href = "dashboard.html";
+
+})
+
+.catch((error) => {
+
+document.getElementById("msg").innerText = error.message;
+
+});
+
+}
+
+
+window.login = login;
