12ebf6e16adf265ac29bf18ed927cc86c76cbcf1
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-13 11:44:43 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,22 @@
 // importar funções do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-
-// configuração do projeto
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-  apiKey: "SUA_API_KEY",
-
-  authDomain: "SEU_DOMINIO",
-
-  projectId: "SEU_PROJECT_ID",
-
-  storageBucket: "SEU_BUCKET",
-
-  messagingSenderId: "SEU_ID",
-
-  appId: "SEU_APP_ID"
-
+  apiKey: "AIzaSyCeQD2PNlXf2j8lTbit6ktOZR2FtAufLbY",
+  authDomain: "agendapro-788d0.firebaseapp.com",
+  projectId: "agendapro-788d0",
+  storageBucket: "agendapro-788d0.firebasestorage.app",
+  messagingSenderId: "627296929583",
+  appId: "1:627296929583:web:a3c247f73748154e1a43da",
+  measurementId: "G-81K2L2E8H5"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // iniciar Firebase
 const app = initializeApp(firebaseConfig);
